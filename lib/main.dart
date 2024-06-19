f1afccc90435dcd5de56a9256ac59acd03a33fbe
Joey Boy Mission <joeyboy.mission@tup.edu.ph>
2024-06-19 13:45:03 +0800

    Section 1: First Flutter Application

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Center(
+      child: Text(
+        'Hello, world!',
+        textAlign: TextAlign.center, // Center the text horizontally
+      ),
+    ),
+  ));
+}
